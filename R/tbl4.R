4a36ea2d4b5ac90f07e0a9d7d9cbd9655c246c16
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2015-11-12 09:15:01 -0200

    Correcao bug ERRO_ACUMULADO

--- R/tbl4.R
+++ R/tbl4.R
@@ -1,40 +1,40 @@
 source("R/clean-data.R")
 
-dt_tbl4_1 <- dt[OBS %in% 1, .(ERRO_ABSOLUTO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(PREVISTO))), by = c("MODELO", "ESTADO")]
-dt_tbl4_1 <- dt_tbl4_1[dt_tbl4_1[, .I[which.min(abs(ERRO_ABSOLUTO))], by=ESTADO]$V1][, .N, by = MODELO]
+dt_tbl4_1 <- dt[OBS %in% 1, .(ERRO_ACUMULADO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(REALIZADO))), by = c("MODELO", "ESTADO")]
+dt_tbl4_1 <- dt_tbl4_1[dt_tbl4_1[, .I[which.min(abs(ERRO_ACUMULADO))], by=ESTADO]$V1][, .N, by = MODELO]
 dt_tbl4_1[, "n=1" := (N / sum(N))]
 dt_tbl4_1[, N := NULL]
 
 
-dt_tbl4_2 <- dt[OBS %in% 1:4, .(ERRO_ABSOLUTO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(PREVISTO))), by = c("MODELO", "ESTADO")]
-dt_tbl4_2 <- dt_tbl4_2[dt_tbl4_2[, .I[which.min(abs(ERRO_ABSOLUTO))], by=ESTADO]$V1][, .N, by = MODELO]
+dt_tbl4_2 <- dt[OBS %in% 1:4, .(ERRO_ACUMULADO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(REALIZADO))), by = c("MODELO", "ESTADO")]
+dt_tbl4_2 <- dt_tbl4_2[dt_tbl4_2[, .I[which.min(abs(ERRO_ACUMULADO))], by=ESTADO]$V1][, .N, by = MODELO]
 dt_tbl4_2[, "n=1:4" := (N / sum(N))]
 dt_tbl4_2[, N := NULL]
 
 
-dt_tbl4_3 <- dt[OBS %in% 5:8, .(ERRO_ABSOLUTO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(PREVISTO))), by = c("MODELO", "ESTADO")]
-dt_tbl4_3 <- dt_tbl4_3[dt_tbl4_3[, .I[which.min(abs(ERRO_ABSOLUTO))], by=ESTADO]$V1][, .N, by = MODELO]
+dt_tbl4_3 <- dt[OBS %in% 5:8, .(ERRO_ACUMULADO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(REALIZADO))), by = c("MODELO", "ESTADO")]
+dt_tbl4_3 <- dt_tbl4_3[dt_tbl4_3[, .I[which.min(abs(ERRO_ACUMULADO))], by=ESTADO]$V1][, .N, by = MODELO]
 dt_tbl4_3[, "n=5:8" := (N / sum(N))]
 dt_tbl4_3[, N := NULL]
 
 
-dt_tbl4_4 <- dt[OBS %in% 9:12, .(ERRO_ABSOLUTO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(PREVISTO))), by = c("MODELO", "ESTADO")]
-dt_tbl4_4 <- dt_tbl4_4[dt_tbl4_4[, .I[which.min(abs(ERRO_ABSOLUTO))], by=ESTADO]$V1][, .N, by = MODELO]
+dt_tbl4_4 <- dt[OBS %in% 9:12, .(ERRO_ACUMULADO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(REALIZADO))), by = c("MODELO", "ESTADO")]
+dt_tbl4_4 <- dt_tbl4_4[dt_tbl4_4[, .I[which.min(abs(ERRO_ACUMULADO))], by=ESTADO]$V1][, .N, by = MODELO]
 dt_tbl4_4[, "n=9:12" := (N / sum(N))]
 dt_tbl4_4[, N := NULL]
 
-dt_tbl4_5 <- dt[OBS %in% 13:16, .(ERRO_ABSOLUTO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(PREVISTO))), by = c("MODELO", "ESTADO")]
-dt_tbl4_5 <- dt_tbl4_5[dt_tbl4_5[, .I[which.min(abs(ERRO_ABSOLUTO))], by=ESTADO]$V1][, .N, by = MODELO]
+dt_tbl4_5 <- dt[OBS %in% 13:16, .(ERRO_ACUMULADO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(REALIZADO))), by = c("MODELO", "ESTADO")]
+dt_tbl4_5 <- dt_tbl4_5[dt_tbl4_5[, .I[which.min(abs(ERRO_ACUMULADO))], by=ESTADO]$V1][, .N, by = MODELO]
 dt_tbl4_5[, "n=13:16" := (N / sum(N))]
 dt_tbl4_5[, N := NULL]
 
-dt_tbl4_6 <- dt[OBS %in% 17:20, .(ERRO_ABSOLUTO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(PREVISTO))), by = c("MODELO", "ESTADO")]
-dt_tbl4_6 <- dt_tbl4_6[dt_tbl4_6[, .I[which.min(abs(ERRO_ABSOLUTO))], by=ESTADO]$V1][, .N, by = MODELO]
+dt_tbl4_6 <- dt[OBS %in% 17:20, .(ERRO_ACUMULADO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(REALIZADO))), by = c("MODELO", "ESTADO")]
+dt_tbl4_6 <- dt_tbl4_6[dt_tbl4_6[, .I[which.min(abs(ERRO_ACUMULADO))], by=ESTADO]$V1][, .N, by = MODELO]
 dt_tbl4_6[, "n=17:20" := (N / sum(N))]
 dt_tbl4_6[, N := NULL]
 
-dt_tbl4_7 <- dt[OBS %in% 1:20, .(ERRO_ABSOLUTO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(PREVISTO))), by = c("MODELO", "ESTADO")]
-dt_tbl4_7 <- dt_tbl4_7[dt_tbl4_7[, .I[which.min(abs(ERRO_ABSOLUTO))], by=ESTADO]$V1][, .N, by = MODELO]
+dt_tbl4_7 <- dt[OBS %in% 1:20, .(ERRO_ACUMULADO = ((sum(REALIZADO) - sum(PREVISTO)) / sum(REALIZADO))), by = c("MODELO", "ESTADO")]
+dt_tbl4_7 <- dt_tbl4_7[dt_tbl4_7[, .I[which.min(abs(ERRO_ACUMULADO))], by=ESTADO]$V1][, .N, by = MODELO]
 dt_tbl4_7[, "n=1:20" := (N / sum(N))]
 dt_tbl4_7[, N := NULL]
 
